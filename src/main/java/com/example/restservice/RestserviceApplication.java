b5ac697a3a7aad90dfacf6e551d1f854ee008761
JuliusHuttunen <63333652+JuliusHuttunen@users.noreply.github.com>
2020-11-18 13:20:23 +0200

    Update RestserviceApplication.java

--- src/main/java/com/example/restservice/RestserviceApplication.java
+++ src/main/java/com/example/restservice/RestserviceApplication.java
@@ -23,16 +23,16 @@ public class RestserviceApplication {
 	@Bean
 	public CommandLineRunner demo(QuestionRepository qrepository, AnswerRepository arepository, UserRepo userrepo) {
 		return (args) -> {
-			qrepository.save(new Question("How old are you?", false));
-			qrepository.save(new Question("How would you rate the course on a scale from 1 to 5?", true));
+			//qrepository.save(new Question("How old are you?", false));
+			//qrepository.save(new Question("How would you rate the course on a scale from 1 to 5?", true));
 
-			arepository.save(new Answer("I am 23 years old"));
-			arepository.save(new Answer("5"));
+			//arepository.save(new Answer("I am 23 years old"));
+			//arepository.save(new Answer("5"));
 
-			User user1 = new User("user", "$2y$12$6LmFjyVVsZqwE6H.r3NFWeqlZ/wh/fN/jLnpUQrkzuhg1LF1raLPe", "USER");
-			User user2 = new User("admin", "$2y$10$5ZVBx3XDH6i6FdSltNYIIeJCcw/nH33wKNi7BEvm.8sKIMP7lITWe", "ADMIN");
-			userrepo.save(user1);
-			userrepo.save(user2);
+			//User user1 = new User("user", "$2y$12$6LmFjyVVsZqwE6H.r3NFWeqlZ/wh/fN/jLnpUQrkzuhg1LF1raLPe", "USER");
+			//User user2 = new User("admin", "$2y$10$5ZVBx3XDH6i6FdSltNYIIeJCcw/nH33wKNi7BEvm.8sKIMP7lITWe", "ADMIN");
+			//userrepo.save(user1);
+			//userrepo.save(user2);
 
 		};
 
