72b76dc5f68d3c97f076aaf5989520bd6c7a781a
JuliusHuttunen <63333652+JuliusHuttunen@users.noreply.github.com>
2020-11-18 13:47:14 +0200

    Update User.java

--- src/main/java/com/example/restservice/domain/User.java
+++ src/main/java/com/example/restservice/domain/User.java
@@ -1,12 +1,14 @@
 package com.example.restservice.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Table;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Table(name="usertable")
 public class User {
 
 	@Id
