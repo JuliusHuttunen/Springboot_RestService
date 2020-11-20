0682c1fc3052d71bf49cfe84f73b46179652f67f
JuliusHuttunen <huttunen.julius@gmail.com>
2020-11-20 14:07:03 +0200

    security edit

--- src/main/java/com/example/restservice/WebConfig.java
+++ src/main/java/com/example/restservice/WebConfig.java
@@ -20,7 +20,9 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("css/**").permitAll().and().authorizeRequests().anyRequest()
+		http.authorizeRequests().antMatchers("css/**").permitAll()
+		.antMatchers("/api").permitAll()
+		.and().authorizeRequests().anyRequest()
 				.authenticated().and().formLogin().defaultSuccessUrl("/question").permitAll().and().logout()
 				.permitAll();
 	}
