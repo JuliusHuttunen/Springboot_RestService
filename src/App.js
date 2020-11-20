d265167b65e0620f23eacad78504ae855ea13d8e
Julius H <63333652+JuliusHuttunen@users.noreply.github.com>
2020-11-20 14:23:18 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Questionlist from './components/Questionlist';
 
 function App() {
   return (
     <div className="App">
-
+      <Questionlist/>
     </div>
   );
 }
