f0650b686c2eb92afd77390fe3e67241f09abac0
Julius H <63333652+JuliusHuttunen@users.noreply.github.com>
2020-11-20 16:34:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 import Questionlist from './components/Questionlist';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <div className="App">
+       <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Kysely
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <Questionlist/>
     </div>
   );
