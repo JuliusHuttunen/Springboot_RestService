2865083c7a1b4b9c3a9b4845ab960ed8fbdc9f88
JuliusHuttunen <huttunen.julius@gmail.com>
2020-11-21 13:50:58 +0200

    commented out security

--- src/main/java/com/example/restservice/web/UserDetail.java
+++ src/main/java/com/example/restservice/web/UserDetail.java
@@ -1,29 +1,29 @@
-package com.example.restservice.web;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import com.example.restservice.domain.User;
-import com.example.restservice.domain.UserRepo;
-
-@Service
-public class UserDetail implements UserDetailsService {
-	private final UserRepo repo;
-
-	@Autowired
-	public UserDetail(UserRepo userrepo) {
-		this.repo = userrepo;
-	}
-
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User curruser = repo.findByUsername(username);
-		UserDetails user = new org.springframework.security.core.userdetails.User(username, curruser.getPasswordHash(),
-				AuthorityUtils.createAuthorityList(curruser.getRole()));
-		return user;
-	}
-}
+//package com.example.restservice.web;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.authority.AuthorityUtils;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import com.example.restservice.domain.User;
+//import com.example.restservice.domain.UserRepo;
+//
+//@Service
+//public class UserDetail implements UserDetailsService {
+//	private final UserRepo repo;
+//
+//	@Autowired
+//	public UserDetail(UserRepo userrepo) {
+//		this.repo = userrepo;
+//	}
+//
+//	@Override
+//	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//		User curruser = repo.findByUsername(username);
+//		UserDetails user = new org.springframework.security.core.userdetails.User(username, curruser.getPasswordHash(),
+//				AuthorityUtils.createAuthorityList(curruser.getRole()));
+//		return user;
+//	}
+//}
