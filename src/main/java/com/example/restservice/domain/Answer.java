2544f49a634c1b5a3e3fd0b36ed095c812559bfe
JuliusHuttunen <huttunen.julius@gmail.com>
2020-12-14 12:10:04 +0200

    testiajo

--- src/main/java/com/example/restservice/domain/Answer.java
+++ src/main/java/com/example/restservice/domain/Answer.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 public class Answer {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
 	private String qst;
